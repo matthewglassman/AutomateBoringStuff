7e37a50db2efa9fdc076bad38b3b9dbcf57a0f81
Matthew Glassman <mglassmn@email.unc.edu>
2021-03-09 14:27:19 -0500

    Added separate folder for Practice Projects and first project

---
+++ Practice Projects/strongpassword.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env/ python3
+
+#A function to use regular expressions to check whether a password inputted from user is considered strong
+
+'''Password is strong if it is at least 8 characters long, 
+contains both upper and lower cases, 1 digit
+and one special character'''
+
+#TODO: Take in user input and assign to a variable
+
+#TODO: Check input for length equal to or greater than 8.
+
+#TODO: Check input for at least one number.
+
+#TODO: Check input for at least one special character.
+
+#TODO: Check input for at least 1 upper and 1 lower case alpha character.
+
+#TODO: If input passes all checks send message to user that password is strong.
