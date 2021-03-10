42ecedbf5b71e5c6f473ea5595a80768351bb4ac
Matthew Glassman <mglassmn@email.unc.edu>
2021-03-10 11:06:31 -0500

    Added variable to take user input and hold password to check

--- Practice Projects/strongpassword.py
+++ Practice Projects/strongpassword.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env/ python3
+#!/usr/bin/python3
 
 #A function to use regular expressions to check whether a password inputted from user is considered strong
 
@@ -7,6 +7,7 @@ contains both upper and lower cases, 1 digit
 and one special character'''
 
 #TODO: Take in user input and assign to a variable
+password = input("Please type in a strong password")
 
 #TODO: Check input for length equal to or greater than 8.
 
