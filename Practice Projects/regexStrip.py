b6ff8bb54fd89b1b73564844ce5d4054a6ba9d46
Matthew Glassman <mglassmn@email.unc.edu>
2021-04-12 20:48:33 -0400

    Skeleton for using RegEx to simulate strip method.

---
+++ Practice Projects/regexStrip.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+''' A program that mimics the effects of the strip() string method
+    but uses Regular Expressions '''
+
+#TODO: Write function that takes a string surrounded by white space
+#	   and strips the white space from it based on Regular Expression 
+#      Search and replace.
+
+#TODO: Write function that takes a string and an optional second parameter
+#      And removes the occurrences of the 2nd parameter from the string using
+#      Regular Expressions
